ab5b5d3e798dafb3e401fce28b6d36cb1c520d28
Mehrez Ben Mohamed <mehrez.mbm@gmail.com>
2019-02-26 21:06:21 +0100

    First commit

---
+++ src/test/java/com/magicretro/MagicRetro/MagicRetroApplicationTests.java
@@ -0,0 +1,16 @@
+package com.magicretro.MagicRetro;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MagicRetroApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
