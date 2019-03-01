430e6a5897f75d87e3109b84354d58b89c8f571e
Mehrez Ben Mohamed <mehrez.mbm@gmail.com>
2019-03-01 22:52:10 +0100

    Add Board, Column and Item table

--- src/main/java/com/magicretro/MagicRetroApplication.java
+++ src/main/java/com/magicretro/MagicRetroApplication.java
@@ -6,8 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class MagicRetroApplication {
 
+
+	
 	public static void main(String[] args) {
 		SpringApplication.run(MagicRetroApplication.class, args);
+		
 	}
-
 }
